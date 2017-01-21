d7fdac715e486bc56286e9cebedf74c278562f48
plok74122 <plok74122@gmail.com>
2017-01-21 19:15:24 +0800

    add action show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,11 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(params.require(:event).permit(:name, :description))
     if @event.save
-      redirect_to :controller => 'events' , :action => 'show'
+      redirect_to :controller => 'events' , :action => 'show' , :id => @event
     end
+  end
+
+  def show
+    @event = Event.find(params[:id])
   end
 end
