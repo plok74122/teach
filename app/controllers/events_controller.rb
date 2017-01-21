b168426d2b274707b5d46f6eda081cc90d1f33e0
plok74122 <plok74122@gmail.com>
2017-01-21 19:08:45 +0800

    add action new create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,17 @@
 class EventsController < ApplicationController
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params.require(:event).permit(:name, :description))
+    if @event.save
+      redirect_to :controller => 'events' , :action => 'show'
+    end
   end
 end
