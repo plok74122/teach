394b63705ff5330eb52bfe4e2422e546ac9271a0
plok74122 <plok74122@gmail.com>
2017-02-02 19:13:26 +0800

    add method show edit update destroy

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -1,5 +1,6 @@
 class EventAttendeesController < ApplicationController
   before_action :set_event
+  before_action :set_attendee, :only => [:show, :edit, :update, :destroy]
 
   def index
     @attendees = @event.attendees
@@ -19,7 +20,34 @@ class EventAttendeesController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @attendee.update(params_attendee)
+      flash[:notice] ='修改成功'
+      redirect_to event_attendee_path(@event, @attendee)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @attendee.destroy
+    flash[:alert] ='刪除成功'
+    redirect_to event_attendees_path(@event)
+  end
+
   private
+  def set_attendee
+    @attendee = @event.attendees.find(params[:id])
+  end
+
   def set_event
     @event = Event.find(params[:event_id])
   end
